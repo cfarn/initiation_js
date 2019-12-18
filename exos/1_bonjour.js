e943249873ae74c91d3b19addf939e6acdf271a0
cfarn <58476975+cfarn@users.noreply.github.com>
2019-12-18 17:02:24 +0100

    exercices js

---
+++ exos/1_bonjour.js
@@ -0,0 +1,8 @@
+var name=prompt('Quel est votre nom ?')
+var firstname=prompt('Quel est votre prénom ?');
+alert('Bonjour' + name + '!');
+alert('Bonjour' + firstname + '!');
+
+
+
+
---
+++ js/01_base_js.js
@@ -0,0 +1,17 @@
+/****** SYNTAXE DE BASE ******/
+
+// 1. Je vérifie toujours que ma page js et mon fichier html sont connectés
+alert('okay JS');
+
+// pour un com sur une ligne
+
+/* 
+(shift+alt+a)
+ici un com sur plusieurs lignes
+*/
+
+/* 
+2 manières d'afficher une boîte de dialogue :
+    => alert('Super, tu es arrivée sur mon site !');
+    => prompt('Question : Quel jour est-on ?'); avec la fonction prompt, je dialogue directement avec l'utilisateur
+*/
